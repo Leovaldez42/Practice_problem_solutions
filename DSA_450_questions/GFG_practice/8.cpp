c8b9be2778d64e305d124468c52a11f170c4a4b3
Leovaldez42 <gaurav4.037@gmail.com>
2020-12-30 17:39:11 +0530

    added largest sum array

--- DSA_450_questions/GFG_practice/8.cpp
+++ DSA_450_questions/GFG_practice/8.cpp
@@ -9,12 +9,12 @@ using namespace std;
 // Function to find subarray with maximum sum
 // arr: input array
 // n: size of array
-int maxSubarraySum(int arr[], int n){
+int maxSubarraySum(int a[], int n){
     
    int max_so_far = a[0]; 
    int curr_max = a[0]; 
   
-   for (int i = 1; i < size; i++) { 
+   for (int i = 1; i < n; i++) { 
         curr_max = max(a[i], curr_max+a[i]); 
         max_so_far = max(max_so_far, curr_max); 
    } 
